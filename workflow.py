a5bb1d27638d91eadaf17ad5609965916020e9fd
mereszeta <grzegorzszeremeta@gmail.com>
2020-01-19 23:50:49 +0100

    changes in components and pipeline

--- workflow.py
+++ workflow.py
@@ -2,11 +2,12 @@ import kfp
 import kfp.components as comp
 import kfp.dsl as dsl
 
-
 ner_op = comp.load_component_from_url('https://raw.githubusercontent.com/mereszeta/kubeflow-workflows/master/ner.yaml')
 help(ner_op)
-sentiment_op = comp.load_component_from_url('https://raw.githubusercontent.com/mereszeta/kubeflow-workflows/master/ner.yaml')
+sentiment_op = comp.load_component_from_url(
+    'https://raw.githubusercontent.com/mereszeta/kubeflow-workflows/master/sentiment.yaml')
 help(sentiment_op)
+
 
 def echo2_op(text1, text2):
     return dsl.ContainerOp(
@@ -16,11 +17,12 @@ def echo2_op(text1, text2):
         arguments=['echo "Ner: $0"; echo "Sentiment: $1"', text1, text2]
     )
 
+
 @dsl.pipeline(
-  name='Nlp kubeflow pipeline',
-  description='A pipeline which finds named entities and calculates sentiment'
+    name='Nlp kubeflow pipeline',
+    description='A pipeline which finds named entities and calculates sentiment'
 )
-def text_transformation_pipeline(inputd, outputd):
-    ner = ner_op(inputd=inputd, outputd=outputd)
-    sentiment = sentiment_op(inputd=inputd, outputd=outputd)
+def text_transformation_pipeline(bucketName, fileCount):
+    ner = ner_op(bucketName, fileCount)
+    sentiment = sentiment_op(bucketName, fileCount)
     echo_task = echo2_op(ner.output, sentiment.output)
